01555dac1e160101e2b8a684e30cb91b5d9d79f8
MadiNithish <madinithish@gmail.com>
2026-06-30 12:22:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 function createTable() {
     //Write your code here
-  
+	const rn = Number(prompt("Input number of rows"));
+	const cn = Number(prompt("Input number of columns"));
+	const table = document.getElementById("myTable");
+  table.innerHTML = "";
+	for(let i = 0; i<rn; i++){
+		const row = table.insertRow();
+		for(let j=0; j<cn; j++){
+			const cell = row.insertCell();
+			cell.innerHTML = `Row-${i} Column-${j}`;
+		}
+	}
 }
